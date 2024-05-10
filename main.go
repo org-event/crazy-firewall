0d6e3288588accb81d472840a80115031c187465
org-event <org.event@ya.ru>
2024-05-10 14:24:12 +0300

    Add logger, add info in README

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 const (
 	configURL      = "https://org-event.github.io/crazy-firewall/config.json" // URL конфигурационного файла
 	updateInterval = 5 * time.Minute                                          // Интервал обновления конфигурации
+	debugging      = true                                                     // Если true, сообщения выводятся в консоли
 )
 
 type Config struct {
@@ -28,29 +29,36 @@ var (
 	configLock = &sync.RWMutex{} // Мьютекс для синхронизированного доступа к конфигурации
 )
 
+// Логирование сообщений, если будет включено
+func logger(format string, v ...interface{}) {
+	if debugging {
+		log.Printf(format, v...)
+	}
+}
+
 // Загружает и обновляет конфигурацию каждые 5 минут
 func fetchConfig() {
 	for {
 		resp, err := http.Get(configURL) // Выполнение HTTP GET запроса
 		if err != nil {
-			log.Printf("Не удалось получить конфигурацию: %v", err)
+			logger("Не удалось получить конфигурацию: %v", err)
 			time.Sleep(updateInterval)
 			continue
 		}
 
 		body, err := ioutil.ReadAll(resp.Body)      // Чтение тела ответа
 		if cerr := resp.Body.Close(); cerr != nil { // Немедленное закрытие тела ответа
-			log.Printf("Не удалось закрыть тело ответа: %v", cerr)
+			logger("Не удалось закрыть тело ответа: %v", cerr)
 		}
 		if err != nil {
-			log.Printf("Не удалось прочитать тело ответа: %v", err)
+			logger("Не удалось прочитать тело ответа: %v", err)
 			time.Sleep(updateInterval)
 			continue
 		}
 
 		var newConfig Config
 		if err := json.Unmarshal(body, &newConfig); err != nil {
-			log.Printf("Не удалось разобрать конфигурацию: %v", err)
+			logger("Не удалось разобрать конфигурацию: %v", err)
 			time.Sleep(updateInterval)
 			continue
 		}
@@ -64,13 +72,13 @@ func fetchConfig() {
 		config = newConfig
 		configLock.Unlock() // Разблокирование доступа
 
-		log.Println("Конфигурация обновлена.")
+		logger("Конфигурация обновлена.")
 		time.Sleep(updateInterval)
 	}
 }
 
 // Проверяет, разрешен ли сайт, включая субдомены
-func isBlockedSite(request string, allowedSites []string) bool {
+func isSiteAllowed(request string, allowedSites []string) bool {
 	requestDomain := extractDomain(request)
 	for _, site := range allowedSites {
 		if requestDomain == site || strings.HasSuffix(requestDomain, "."+site) {
@@ -111,7 +119,7 @@ func isUserAllowed(username string, allowedUsers []string) bool {
 func handleConnection(src net.Conn) {
 	defer func() {
 		if err := src.Close(); err != nil {
-			log.Printf("Ошибка при закрытии соединения: %v", err)
+			logger("Ошибка при закрытии соединения: %v", err)
 		}
 	}()
 
@@ -124,46 +132,46 @@ func handleConnection(src net.Conn) {
 	buffer := make([]byte, 4096)
 	n, err := src.Read(buffer)
 	if err != nil {
-		log.Printf("Ошибка чтения из источника: %v", err)
+		logger("Ошибка чтения из источника: %v", err)
 		return
 	}
 
 	request := string(buffer[:n])
 	if strings.HasPrefix(request, "CONNECT") {
 		destination := request[len("CONNECT "):strings.Index(request, " HTTP/")]
-		if isUserAllowed(username, allowedUsers) && !isBlockedSite(destination, blockedSites) {
-			log.Printf("Запрос заблокирован: %s", destination, username, allowedUsers, " работа ", isUserAllowed(username, allowedUsers))
+		if isUserAllowed(username, allowedUsers) && !isSiteAllowed(destination, blockedSites) {
+			logger("Запрос заблокирован: %s", destination)
 			return
 		}
 
-		log.Printf("Запрос разрешен: %s", destination)
+		logger("Запрос разрешен: %s", destination)
 		dst, err := net.Dial("tcp", destination)
 		if err != nil {
-			log.Printf("Не удалось соединиться с назначением: %v", err)
+			logger("Не удалось соединиться с назначением: %v", err)
 			return
 		}
 		defer func() {
 			if err := dst.Close(); err != nil {
-				log.Printf("Ошибка при закрытии целевого соединения: %v", err)
+				logger("Ошибка при закрытии целевого соединения: %v", err)
 			}
 		}()
 
 		// Отправляет клиенту подтверждение установки соединения
 		if _, err := src.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n")); err != nil {
-			log.Printf("Ошибка при отправке подтверждения соединения: %v", err)
+			logger("Ошибка при отправке подтверждения соединения: %v", err)
 			return
 		}
 
 		// Начинает асинхронное копирование данных от клиента к серверу
 		go func() {
 			if _, err := io.Copy(dst, src); err != nil {
-				log.Printf("Ошибка при копировании данных к серверу: %v", err)
+				logger("Ошибка при копировании данных к серверу: %v", err)
 			}
 		}()
 
 		// Копирует данные от сервера к клиенту
 		if _, err := io.Copy(src, dst); err != nil {
-			log.Printf("Ошибка при копировании данных к клиенту: %v", err)
+			logger("Ошибка при копировании данных к клиенту: %v", err)
 		}
 	}
 }
@@ -176,12 +184,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Не удалось прослушать порт: %v", err)
 	}
-	log.Println("Прослушивание порта :20000")
+	logger("Прослушивание порта :20000")
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Не удалось принять соединение: %v", err)
+			logger("Не удалось принять соединение: %v", err)
 			continue
 		}
 		go handleConnection(conn)
